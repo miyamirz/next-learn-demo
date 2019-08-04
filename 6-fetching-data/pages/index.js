73b2fd6ef8223204ef4121db4f619be9778f6f13
Miya Mirza <miyamirza.esc@gmail.com>
2019-08-04 10:12:40 +0530

    done

--- 6-fetching-data/pages/index.js
+++ 6-fetching-data/pages/index.js
@@ -1,23 +1,39 @@
-import Layout from '../components/MyLayout.js'
+import Layout from '../components/MyLayout'
 import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
+import React from 'react'
+import { useRouter } from 'next/router'
 
-const PostLink = props => (
-  <li>
-    <Link href="/p/[id]" as={`/p/${props.id}`}>
-      <a>{props.id}</a>
-    </Link>
-  </li>
-)
-
-export default function Blog() {
+function Index(props) {
+  console.log('Rendering------------------------------')
   return (
     <Layout>
-      <h1>My Blog</h1>
+      <h1>Batman TV Shows</h1>
       <ul>
-        <PostLink id="hello-nextjs" />
-        <PostLink id="learn-nextjs" />
-        <PostLink id="deploy-nextjs" />
+        {props.shows.map(show => (
+          <li key={show.id}>
+            <Link href="/p/[id]" as={`/p/${show.id}`}>
+              <a>{show.name}</a>
+            </Link>
+          </li>
+        ))}
       </ul>
     </Layout>
   )
 }
+
+Index.getInitialProps = async function() {
+  // const router = useRouter()
+  // console.log(router)
+  console.log('getting------')
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+  const data = await res.json()
+
+  console.log(`Show data fetched. Count: ${data.length}`)
+
+  return {
+    shows: data.map(entry => entry.show)
+  }
+}
+
+export default Index
